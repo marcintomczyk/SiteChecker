15c39720dc86978054fae20cb851f9a784122c5d
marcin.tomczyk <marcin.tomczyk@e-qsg.com>
2019-08-01 13:35:29 +0200

    cleaning up the model and migrations

--- site_checker/models/site_checkers.py
+++ site_checker/models/site_checkers.py
@@ -4,11 +4,11 @@ from django.db import models
 class Site(models.Model):
     # for the current project it might seem unnecessary to have urlfield as we have validation on the view
     # but for bigger projects there might be other 'checkers' without validation on view layer
-    original_address = models.URLField(max_length=200, null=True, blank=True)
-    final_address = models.URLField(max_length=200, null=True, blank=True)
+    original_address = models.URLField(max_length=200)
+    final_address = models.URLField(max_length=200, blank=True)
     status_code = models.PositiveSmallIntegerField()
     check_date = models.DateTimeField('check date')
-    error_message = models.TextField(max_length=400, null=True)
+    error_message = models.TextField(max_length=400, blank=True)
 
     def __str__(self):
         return self.original_address
