24c532969219705ac4b80034223c686ee90e9c87
marcin.tomczyk <marcin.tomczyk@e-qsg.com>
2019-08-24 22:07:11 +0200

    adding communication via django channels

--- site_checker/views/site_checkers.py
+++ site_checker/views/site_checkers.py
@@ -1,17 +1,22 @@
+
 import asyncio
 from datetime import datetime
 from threading import Thread
 
 import aiohttp
+import json
 from aiohttp import ClientConnectionError
 from django.core.exceptions import ValidationError
 from django.core.validators import URLValidator
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.utils import timezone
 
 from site_checker.models import Site
 
 SITE_CHECKER_MAIN_URL = 'site_checker/checker.html'
+SITE_CHECKER_CHANNEL_MAIN_ROOM = 'ws://localhost:8000/ws/checker_channel/main_room/'
+
 
 
 # yes, I know I could use Class-based views :) but haven't used them intentionally
@@ -22,10 +27,29 @@ def index(request):
 def start_loop(loop, original_site_address):
     asyncio.set_event_loop(loop)
     site_response = loop.run_until_complete(check_site_status(original_site_address))
+
+    #TODO: introduce chaining
+    loop.run_until_complete(send_status_info(original_site_address, site_response))
+
     create_site_details(original_site_address, site_response)
 
 
+async def send_status_info(url, response):
+    async with aiohttp.ClientSession() as ws_session:
+        async with ws_session.ws_connect(SITE_CHECKER_CHANNEL_MAIN_ROOM) as ws:
+            await ws.send_str(prepare_message(url, "checking finished, status: " + str(response.status)))
+            ws.close()
+        ws_session.close()
+
 async def check_site_status(url):
+    print('sending message to ws - 1')
+
+    async with aiohttp.ClientSession() as ws_session:
+        async with ws_session.ws_connect(SITE_CHECKER_CHANNEL_MAIN_ROOM) as ws:
+            await ws.send_str(prepare_message(url, "preparing for checking site"))
+            ws.close()
+        ws_session.close()
+
     # TODO: introduce logging
     # BUT it requires having correct settings in the main project
     # so without configuring it log.debug's statements would be not visible
@@ -41,6 +65,12 @@ async def check_site_status(url):
     # in where and how this text would be used)
     connector = aiohttp.TCPConnector(verify_ssl=False)
     try:
+        async with aiohttp.ClientSession() as ws_session:
+            async with ws_session.ws_connect(SITE_CHECKER_CHANNEL_MAIN_ROOM) as ws:
+                await ws.send_str(prepare_message(url, "checking site started"))
+                ws.close()
+            ws_session.close()
+
         async with aiohttp.ClientSession(connector=connector) as session:
             return await session.head(url, allow_redirects=True)
     # TODO: we could also catch another exceptions such as HTTPError, Timeouts, TooManyRedirects
@@ -101,7 +131,20 @@ def check(request):
     t = Thread(target=start_loop, args=(loop, site_address))
     t.start()
 
-    return render(request, 'site_checker/checker.html', {
+    #return render(request, 'site_checker/checker.html', {
+    #    'message': "The request was sent for execution",
+    #    'time_of_the_message': timezone.now().strftime("%Y-%m-%d %H:%M:%S")
+    #})
+
+    return HttpResponse(json.dumps({
         'message': "The request was sent for execution",
         'time_of_the_message': timezone.now().strftime("%Y-%m-%d %H:%M:%S")
-    })
+    }))
+
+
+def prepare_message(url, message):
+    request_data = {'url': str(url),
+                    'message': message,
+                    'timestamp': str(timezone.now())
+                    }
+    return json.dumps(request_data)
